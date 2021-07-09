fd178df835592e2424c040ca35a3429370f78c47
Chase Rehm <chase.rehm@golfchannel.com>
2021-07-09 09:08:55 -0400

    Add comments and sterilize mongo connection strings

--- main.py
+++ main.py
@@ -10,57 +10,60 @@ from replit import db
 from keep_alive import keep_alive
 from discord.ext import commands
 
-#Setting all intent to True so certain retrieval of data can work
+# setting all intent to True so certain retrieval of data can work
 intents = discord.Intents.all()
-intents.members = True  # Subscribe to the privileged members intent.
+# subscribe to the privileged members intent.
+intents.members = True
 
-#Private env variables for API/DB access
+# private env variables for API/DB access. If using replit, you will need to create "secrets" in your replit project for each of the variables preceded by 'os.getenv'. The mongo user, password, and string you will get from your mongo account. The mongo string is your connection string for your mongo database. 
 mongo_user = os.getenv('MONGOUSER')
 mongo_pw = os.getenv('MONGOPW')
 mongo_string = os.getenv('MONGOSTRING')
 mongo_client_string = f"mongodb+srv://{mongo_user}:{mongo_pw}@{mongo_string}"
 
-#Initialize Mongo client and db
+# initialize Mongo client and db
 cluster = MongoClient(mongo_client_string)
-mdb = cluster["Reco_List"]
-mdbc = mdb["reco_records"]
+mdb = cluster[<"your mongo cluster>"]
+mdbc = mdb[<"your mongo collection">]
 
-#This is the instance of the client
-client = commands.Bot(command_prefix='$', intents=intents)
+# instance of the client
+client = commands.Bot(command_prefix='reco:', intents=intents)
 
-#This is the event that is created to store the function
+# event that is created to store the function
 @client.event
-#This event is called when the bot is ready to be used.
+# called when the bot is ready to be used.
 async def on_ready():
-	#When the on_ready function is called then this is printed to the console.
+	# when the on_ready function is called then this is printed to the console
 	print('We have logged in as {0.user}'.format(client))
 
+# command for displaying all recommendations. more recommendation types can be added by copy/pasting the embedVar and then editing the <reco type>.
 @client.command()
 async def recolist(ctx):
 	embedVar = discord.Embed(
 	    title="Recommendation List",
 	    description=
-	    'Find below different media suggested by users! If you would like to recommend something, use the .addreco command. Always begin and end your titles with double quotes like this, "Title"',
+	    'Find below different media suggested by users! If you would like to recommend something, use the .addreco command. Always begin and end your titles with double quotes like this, "Title", then follow that with a space and then your reco type',
 	    color=0x00ff00)
-	embedVar.add_field(name="Books",
-	                   value=util.get_recos('book'),
+	embedVar.add_field(name="<reco type">,
+	                   value=util.get_recos('<reco type>'),
 	                   inline=False)
-	embedVar.add_field(name="Movies", 
-                     value=util.get_recos('movie'), 
+	embedVar.add_field(name="<reco type>", 
+                     value=util.get_recos('<reco type>'), 
                      inline=False)
-	embedVar.add_field(name="Television Series", 
-                     value=util.get_recos('television'), 
+	embedVar.add_field(name="<reco type>", 
+                     value=util.get_recos('<reco type>'), 
                      inline=False)
 	await ctx.channel.send(embed=embedVar)
 
-
+# command for adding a reco. example: < reco:addreco "Movie Title" movie >
 @client.command()
 async def addreco(ctx, title, medium):
   author = ctx.message.author.id
   post = {"title":title,"medium":medium,"author":author}
   mdbc.insert_one(post)
 
-
+# command for deleting a reco. recos can only be deleted by the user that added them.
+# example: < reco:delreco "Movie Title" movie >
 @client.command()
 async def delreco(ctx, title, medium):
   author = ctx.message.author.id
@@ -75,14 +78,16 @@ async def delreco(ctx, title, medium):
     else:
       await ctx.channel.send("You didn't post this reco. You can't delete another person's reco.")
   else:
-    await ctx.channel.send("Somethin' weird is happening. Try again or contanct an admin.")
+    await ctx.channel.send("Something weird is happening. Try again or contanct an admin.")
 
+# command for choosing a random recommendation from a given recommendation type.
+# example: < reco:recorandom movie >
 @client.command()
 async def recorandom(ctx, medium):
   choice = util.get_rand_reco(medium)
   await ctx.channel.send(choice)
 
 
-#this is what runs the client overall. The token is stored in the .env file.
+# runs the bot. the token is stored in your replit secrets, similar to prior mongo variables.
 keep_alive()
 client.run(os.getenv('DCTOKEN'))
--- util.py
+++ util.py
@@ -3,17 +3,18 @@ import pymongo
 import random
 from pymongo import MongoClient
 
-#Private env variables for API/DB access
+# private env variables for API/DB access. If using replit, you will need to create "secrets" in your replit project for each of the variables preceded by 'os.getenv'. The mongo user, password, and string you will get from your mongo account. The mongo string is your connection string for your mongo database. 
 mongo_user = os.getenv('MONGOUSER')
 mongo_pw = os.getenv('MONGOPW')
 mongo_string = os.getenv('MONGOSTRING')
 mongo_client_string = f"mongodb+srv://{mongo_user}:{mongo_pw}@{mongo_string}"
 
-#Initialize Mongo client and db
+# initialize Mongo client and db
 cluster = MongoClient(mongo_client_string)
-mdb = cluster["Reco_List"]
-mdbc = mdb["reco_records"]
+mdb = cluster[<"your mongo cluster>"]
+mdbc = mdb[<"your mongo collection">]
 
+# function used when retrieving and displaying all recommendations
 def get_recos(medium):
   """Finds all recommendation titles with the given medium"""
   matches = mdbc.find({"medium":medium})
@@ -24,6 +25,7 @@ def get_recos(medium):
     s = ', '.join(title_list)
   return s
 
+# function used when retrieving a random recommendation from given medium
 def get_rand_reco(medium):
   """Grabs a random recommendation from the given medium"""
   matches = mdbc.find({"medium":medium})
